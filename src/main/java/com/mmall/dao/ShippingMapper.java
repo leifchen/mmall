50d98c23d56b865337afc9f66d78217e2cc760fa
LeifChen <leifchen90@gmail.com>
2019-03-05 14:54:26 +0800

    feat: add shipping module

--- src/main/java/com/mmall/dao/ShippingMapper.java
+++ src/main/java/com/mmall/dao/ShippingMapper.java
@@ -1,6 +1,9 @@
 package com.mmall.dao;
 
 import com.mmall.pojo.Shipping;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 /**
  * 物流信息Mapper
@@ -50,4 +53,26 @@ public interface ShippingMapper {
      * @return
      */
     int updateByPrimaryKey(Shipping record);
+
+    /**
+     * 更新收货地址信息
+     * @param shipping 收货地址信息
+     * @return
+     */
+    int updateByShipping(Shipping shipping);
+
+    /**
+     * 根据收货地址id和用户id查询
+     * @param shippingId 收货地址id
+     * @param userId     用户id
+     * @return
+     */
+    Shipping selectByShippingIdAndUserId(@Param("shippingId") Integer shippingId, @Param("userId") Integer userId);
+
+    /**
+     * 根据用户id查询
+     * @param userId 用户id
+     * @return
+     */
+    List<Shipping> selectByUserId(Integer userId);
 }

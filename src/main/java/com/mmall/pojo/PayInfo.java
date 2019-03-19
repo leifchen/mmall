736c6d84c889a8582b55f7b4b089c6781366113a
LeifChen <leifchen90@gmail.com>
2019-03-19 08:32:32 +0800

    chore: add lombok

--- src/main/java/com/mmall/pojo/PayInfo.java
+++ src/main/java/com/mmall/pojo/PayInfo.java
@@ -1,5 +1,10 @@
 package com.mmall.pojo;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.util.Date;
 
 /**
@@ -8,6 +13,10 @@ import java.util.Date;
  * @Author LeifChen
  * @Date 2019-02-25
  */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class PayInfo {
     private Integer id;
 
@@ -24,83 +33,4 @@ public class PayInfo {
     private Date createTime;
 
     private Date updateTime;
-
-    public PayInfo(Integer id, Integer userId, Long orderNo, Integer payPlatform, String platformNumber, String platformStatus, Date createTime, Date updateTime) {
-        this.id = id;
-        this.userId = userId;
-        this.orderNo = orderNo;
-        this.payPlatform = payPlatform;
-        this.platformNumber = platformNumber;
-        this.platformStatus = platformStatus;
-        this.createTime = createTime;
-        this.updateTime = updateTime;
-    }
-
-    public PayInfo() {
-        super();
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public Integer getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Integer userId) {
-        this.userId = userId;
-    }
-
-    public Long getOrderNo() {
-        return orderNo;
-    }
-
-    public void setOrderNo(Long orderNo) {
-        this.orderNo = orderNo;
-    }
-
-    public Integer getPayPlatform() {
-        return payPlatform;
-    }
-
-    public void setPayPlatform(Integer payPlatform) {
-        this.payPlatform = payPlatform;
-    }
-
-    public String getPlatformNumber() {
-        return platformNumber;
-    }
-
-    public void setPlatformNumber(String platformNumber) {
-        this.platformNumber = platformNumber == null ? null : platformNumber.trim();
-    }
-
-    public String getPlatformStatus() {
-        return platformStatus;
-    }
-
-    public void setPlatformStatus(String platformStatus) {
-        this.platformStatus = platformStatus == null ? null : platformStatus.trim();
-    }
-
-    public Date getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(Date createTime) {
-        this.createTime = createTime;
-    }
-
-    public Date getUpdateTime() {
-        return updateTime;
-    }
-
-    public void setUpdateTime(Date updateTime) {
-        this.updateTime = updateTime;
-    }
 }
